880977dcc4e35e0b962a80197f2d6a2a4cb0ca0c
nassim <nassim@nassim-Y13G012S4EI>
2026-06-01 16:10:20 +0200

    màj

--- src/fr/uge/but/schtroumpf/controller/ChoixDifficulteController.java
+++ src/fr/uge/but/schtroumpf/controller/ChoixDifficulteController.java
@@ -5,10 +5,10 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.Node;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
@@ -21,7 +21,6 @@ public class ChoixDifficulteController {
 
     @FXML
     private void retourMenu(ActionEvent event) throws IOException {
-
         Parent root = FXMLLoader.load(
             getClass().getResource("/fr/uge/but/schtroumpf/view/menu.fxml")
         );
@@ -29,12 +28,9 @@ public class ChoixDifficulteController {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
         Scene scene = new Scene(root, 900, 600);
-
-        String css = getClass()
-                .getResource("/fr/uge/but/schtroumpf/view/style.css")
-                .toExternalForm();
-
-        scene.getStylesheets().add(css);
+        scene.getStylesheets().add(
+            getClass().getResource("/fr/uge/but/schtroumpf/view/style.css").toExternalForm()
+        );
 
         stage.setScene(scene);
         stage.show();
@@ -42,7 +38,6 @@ public class ChoixDifficulteController {
 
     @FXML
     private void validerChoix(ActionEvent event) throws IOException {
-
         if (difficulteChoisie == null) {
             System.out.println("Aucune difficulté sélectionnée !");
             return;
@@ -50,19 +45,23 @@ public class ChoixDifficulteController {
 
         System.out.println("Difficulté validée : " + difficulteChoisie);
 
-        Parent root = FXMLLoader.load(
+        FXMLLoader loader = new FXMLLoader(
             getClass().getResource("/fr/uge/but/schtroumpf/view/interface.fxml")
         );
+
+        Parent root = loader.load();
+
+        GameController gameController = loader.getController();
+
+        // Écran affiché après le choix de difficulté
+        gameController.afficherRessourceCollect();
 
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
         Scene scene = new Scene(root, 900, 600);
-
-        String css = getClass()
-                .getResource("/fr/uge/but/schtroumpf/view/style.css")
-                .toExternalForm();
-
-        scene.getStylesheets().add(css);
+        scene.getStylesheets().add(
+            getClass().getResource("/fr/uge/but/schtroumpf/view/style.css").toExternalForm()
+        );
 
         stage.setScene(scene);
         stage.show();
@@ -72,7 +71,6 @@ public class ChoixDifficulteController {
     private void cardSelected(MouseEvent event) {
         Node carteCliquee = (Node) event.getSource();
 
-        // 1. Toutes les cartes repassent en OFF
         for (Node carte : difficultyContainer.getChildren()) {
             carte.getStyleClass().remove("difficulty_card_on");
 
@@ -81,14 +79,12 @@ public class ChoixDifficulteController {
             }
         }
 
-        // 2. La carte cliquée passe en ON
         carteCliquee.getStyleClass().remove("difficulty_card_off");
 
         if (!carteCliquee.getStyleClass().contains("difficulty_card_on")) {
             carteCliquee.getStyleClass().add("difficulty_card_on");
         }
 
-        // 3. On récupère l'id de la carte
         difficulteChoisie = carteCliquee.getId();
 
         System.out.println("Carte cliquée : " + difficulteChoisie);
--- src/fr/uge/but/schtroumpf/controller/GameController.java
+++ src/fr/uge/but/schtroumpf/controller/GameController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 
@@ -27,29 +28,67 @@ public class GameController {
     @FXML
     private VBox cardGrandSchtroumpf;
 
+    @FXML
+    private Label c1;
+
+    @FXML
+    private Label c2;
+
+    @FXML
+    private Label c3;
+
     private String smurfChoisi = "gourmand";
+
+    private String choixAction;
+
+    public void afficherConseil() {
+        chargerDansGameSection("/fr/uge/but/schtroumpf/view/conseil.fxml");
+    }
+
+    public void afficherRessourceCollect() {
+        chargerDansGameSection("/fr/uge/but/schtroumpf/view/ressourceCollect.fxml");
+    }
+
+    public void afficherSelectSmurfAction() {
+        chargerDansGameSection("/fr/uge/but/schtroumpf/view/selectSmurfAction.fxml");
+    }
+
+    public void afficherSmurfActionResult() {
+        chargerDansGameSection("/fr/uge/but/schtroumpf/view/smurfActionResult.fxml");
+    }
+
+    @FXML
+    private void conseil(ActionEvent event) {
+        afficherConseil();
+    }
+
+    @FXML
+    private void ressourceCollect(ActionEvent event) {
+        afficherRessourceCollect();
+    }
 
     @FXML
     private void selectSmurfAction(ActionEvent event) {
-        try {
-            VBox nouvelleBox = FXMLLoader.load(
-                getClass().getResource("/fr/uge/but/schtroumpf/view/selectSmurfAction.fxml")
-            );
-
-            gameSection.getChildren().clear();
-            gameSection.getChildren().add(nouvelleBox);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        afficherSelectSmurfAction();
     }
 
     @FXML
     private void smurfActionResult(ActionEvent event) {
+        if (choixAction == null) {
+            System.out.println("Aucune action sélectionnée !");
+            return;
+        }
+
+        System.out.println("Action validée : " + choixAction);
+        afficherSmurfActionResult();
+    }
+
+    private void chargerDansGameSection(String cheminFXML) {
         try {
-            VBox nouvelleBox = FXMLLoader.load(
-                getClass().getResource("/fr/uge/but/schtroumpf/view/smurfActionResult.fxml")
-            );
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(cheminFXML));
+            loader.setController(this);
+
+            VBox nouvelleBox = loader.load();
 
             gameSection.getChildren().clear();
             gameSection.getChildren().add(nouvelleBox);
@@ -86,7 +125,7 @@ public class GameController {
 
     private void mettreCarteOff(Node carte) {
         if (carte == null) {
-            System.out.println("Carte null : vérifie le fx:id dans le FXML");
+            System.out.println("Carte null : vérifie le fx:id dans conseil.fxml");
             return;
         }
 
@@ -95,5 +134,48 @@ public class GameController {
         if (!carte.getStyleClass().contains("smurf_card_off")) {
             carte.getStyleClass().add("smurf_card_off");
         }
+    }
+
+    @FXML
+    private void choiceSelected(MouseEvent event) {
+        Node choixClique = (Node) event.getSource();
+
+        resetChoices();
+
+        choixClique.getStyleClass().remove("smurf_card_off");
+
+        if (!choixClique.getStyleClass().contains("smurf_card_on")) {
+            choixClique.getStyleClass().add("smurf_card_on");
+        }
+
+        choixAction = choixClique.getId();
+
+        System.out.println("Choix sélectionné : " + choixAction);
+    }
+
+    private void resetChoices() {
+        mettreChoixOff(c1);
+        mettreChoixOff(c2);
+        mettreChoixOff(c3);
+    }
+
+    private void mettreChoixOff(Node choix) {
+        if (choix == null) {
+            return;
+        }
+
+        choix.getStyleClass().remove("smurf_card_on");
+
+        if (!choix.getStyleClass().contains("smurf_card_off")) {
+            choix.getStyleClass().add("smurf_card_off");
+        }
+    }
+
+    public String getSmurfChoisi() {
+        return smurfChoisi;
+    }
+
+    public String getChoixAction() {
+        return choixAction;
     }
 }
